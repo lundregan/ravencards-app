e6481f2809d014da0620627580f07a43810b9ad4
Lundregan <lundregan.ethan@gmail.com>
2024-03-24 14:35:40 +0000

    display current cards by progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	import { ConicGradient } from '@skeletonlabs/skeleton';
 	import MdReplay from 'svelte-icons/md/MdReplay.svelte'
 
-	const conicStops: ConicStop[] = [
-		{ label: 'Not started', color: ['gray', 500], start: 0, end: 76 },
-		{ label: 'Learning', color: ['yellow', 500], start: 76, end: 98 },
-		{ label: 'Mastered', color: ['green', 500], start: 95, end: 100 }
+	$: notStartedEnd = data.cardProgressCounts.notStarted / (data.cardProgressCounts.notStarted + data.cardProgressCounts.learning + data.cardProgressCounts.mastered) * 100 || 0;  // Handle potential division by zero
+	$: learningEnd = notStartedEnd + data.cardProgressCounts.learning / (data.cardProgressCounts.notStarted + data.cardProgressCounts.learning + data.cardProgressCounts.mastered) * 100 || 0;; 
+
+	$: conicStops = [
+		{ label: 'Not started', color: ['gray', 500], start: 0, end: notStartedEnd },
+		{ label: 'Learning', color: ['yellow', 500], start: notStartedEnd, end: learningEnd },
+		{ label: 'Mastered', color: ['green', 500], start: learningEnd, end: 100 }
 	];
 
 	export let data;
@@ -19,8 +24,10 @@
 
 	<div class="flex items-center justify-center h-full">
 		<div class="max-w-xs">
-			<ConicGradient stops={conicStops} legend>Card Progress</ConicGradient>
-		
+			{#if conicStops}
+				<ConicGradient stops={conicStops} legend>Card Progress</ConicGradient>
+			{/if}
+
 			<a href="/review" class="btn variant-outline-primary w-full mt-8">
 				<div class="w-6">
 					<MdReplay />
